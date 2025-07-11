b116222ee76497f828a5b8cb5eedba374f540f2f
Samuel Klutse <klutse.samuel@gmail.com>
2025-07-11 17:36:27 -0400

    Design updates

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,6 @@ import FamilyTree from '../components/FamilyTree';
 
 export default function Page() {
   return (
-    <main>
-      <h1>My Family Tree</h1>
       <FamilyTree />
-    </main>
   );
 }
